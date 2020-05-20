8154198681a6011bae4212bf7c1335374bd23dd0
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2020-05-20 04:07:23 +0200

    commit of the whole spain file

---
+++ covid19_data_spain/merging.py
@@ -0,0 +1,9 @@
+﻿import pandas as pd
+
+df_cases = pd.read_csv("cases_and_hospitalised_and_ICU.csv")
+df_hospitalized = pd.read_csv("ccaa_covid19_altas_long.csv")
+
+df_new = pd.merge(df_cases, df_hospitalized, left_index=True, right_index=True)
+
+df_new.to_csv("cases_and_hospitalised_and_ICU_and_recovered.csv")
+
---
+++ covid19_data_spain/preprocess_covid.py
@@ -0,0 +1,14 @@
+﻿import pandas as pd
+import os
+
+df = pd.read_csv("cases_and_hospitalised_and_ICU_and_recovered.csv")
+
+spain_counties = df['CCAA'].unique()
+
+print(spain_counties)
+
+
+for i in range(len(spain_counties)):
+    new_df = df[df['CCAA'] == spain_counties[i]]
+    
+    new_df.to_csv('covid19_spain_counties/'+spain_counties[i]+'.csv')
